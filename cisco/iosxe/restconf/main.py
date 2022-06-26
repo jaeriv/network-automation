6bb5e314660fbd9332773068618010fbc1e76a6b
Jonathon Rivera <69470875+jaeriv@users.noreply.github.com>
2022-06-26 13:21:16 -0500

    Create main.py

---
+++ cisco/iosxe/restconf/main.py
@@ -0,0 +1,14 @@
+from cisco_restconf import GetDeviceData
+
+
+
+if __name__ == "__main__":
+    data = GetDeviceData(
+        username="", # Username 
+        password="", # Password
+        host="", # Hostname or IP Address
+        port="" # Port
+    )
+    # data.get_yang_capabilities()
+    # data.get_interface_stats()
+    # data.get_interface_config()
